2e75481bcb1c76905e529c1a71524205c8d4851f
Neisvestney <neisvestney113@gmail.com>
2021-02-25 12:30:50 +0300

    Add login and logout

---
+++ trashos_server/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'trashos_server.apps.TrashOSConfig'
--- trashos_server/settings.py
+++ trashos_server/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'trashos_server',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = 'index'
 
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
